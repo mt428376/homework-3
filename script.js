af30e00193129ef6ebdba7b4d3f50d517a3aeec2
mya todd <mya.todd428376@gmail.com>
2020-12-10 23:47:01 -0500

    updated javascript

--- script.js
+++ script.js
@@ -1,13 +1,70 @@
 console.log("hello");
 var generateBtn = document.querySelector("#generate");
-
+var combineArray =[];
+var lowerCase = [
+  "a",
+  "q",
+  "e",
+  ",r",
+  "t",
+  "y",
+  "u",
+  "i",
+  "o",
+  "p",
+  "s",
+  "d",
+  "f",
+  "g",
+  "h",
+  "l",
+  "k",
+  "l",
+  "z",
+  "x",
+  "c",
+  "v",
+  "b",
+  "n",
+  "m",
+];
+var upperCase = [
+  "A",
+  "S",
+  "D",
+  "F",
+  "G",
+  "H",
+  "J",
+  "K",
+  "L",
+  "Z",
+  "X",
+  "C",
+  "V",
+  "B",
+  "N",
+  "M",
+  "Q",
+  "W",
+  "E",
+  "R",
+  "T",
+  "Y",
+  "U",
+  "I",
+  "O",
+  "P",];
+  var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+  var specialCharacters = ["-", "=", ";", ":", "/"];
+var userLength;
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-}
+
 // making up  the password
 function generatePassword() {
   var userLength = prompt(" password length between 8 and 128");
@@ -15,98 +72,48 @@ function generatePassword() {
   {
     alert(" Please enter number between 8 and 128");
   }
-  var lowerCase = confirm("Would you like lower case letters? ");
+  
+  
 
-  var upperCase = confirm(" Would you like lower upper letters?");
+ 
 
-  var numbers = confirm("Would you like numbers?");
+  var lowerCasev = confirm("Would you like lower case letters?");
+  if (lowerCasev === true) {
+    combineArray.push(lowerCase);
+console.log();
+    }
+  
+  
 
-  var specialCharacters = "Would you like special characters?";
-
-  if (wantlowerCase === true) {
-    userCharacterSelections.push(...lowerCase)
+  var upperCasev = confirm(" Would you like lower upper letters?");
+  if (upperCasev === true) {
+   combineArray.push(upperCase);
+   
   }
-  if (wantUpperCase === true) {
-    userCharacterSelections.push(...upperCase);
+  var numbersv = confirm("Would you like numbers?");
+  if (numbersv === true) {
+    combineArray.push(numbers);
+
+   
   }
-  if (wantnumbers === true) {
-    userCharacterSelections.push(...numbers);
+  var specialCharactersv = confirm("Would you like special characters?");
+  if (specialCharactersv === true) {
+    combineArray.push(specialCharacters);
+    console.log(combineArray);
+     
   }
-  if (wantspecialCharacters === true) {
-    userCharacterSelections.push(...specialCharacters);
-  }
 
-  var lowerCase = [
-    "a",
-    "q",
-    "e",
-    ",r",
-    "t",
-    "y",
-    "u",
-    "i",
-    "o",
-    "p",
-    "s",
-    "d",
-    "f",
-    "g",
-    "h",
-    "l",
-    "k",
-    "l",
-    "z",
-    "x",
-    "c",
-    "v",
-    "b",
-    "n",
-    "m",
-  ];
-  var upperCase = [
-    "A",
-    "S",
-    "D",
-    "F",
-    "G",
-    "H",
-    "J",
-    "K",
-    "L",
-    "Z",
-    "X",
-    "C",
-    "V",
-    "B",
-    "N",
-    "M",
-    "Q",
-    "W",
-    "E",
-    "R",
-    "T",
-    "Y",
-    "U",
-    "I",
-    "O",
-    "P",
-  ];
-  var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
-  var specialCharacters = ["-", "=", ";", ":", "/"];
+  
+var random = Math.floor(Math.random() *combineArray.length); 
+  var randomCharacter = Math.floor(Math.random() *combineArray[random].length);
 
-  var randomCharacter = Math.floor(
-    Math.random() * userCharacterSelections.length
-  );
-
-  var password = "";
-
-  for (var i = 0; i < numOfCharacters; i++) {
-    var randomCharacter = Math.floor(
-      Math.random() * userCharacterSelections.length
-    );
-    password += userCharacterSelections[randomCharacter];
+  
+for (var i = 0; i < userLength; i++) {
+  console.log(i);
+    password = password + combineArray[random][randomCharacter];
   }
   return password;
+}
 }
 
 // Add event listener to generate button
